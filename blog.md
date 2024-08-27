5d8b48f4f56a201a4b0c0a552748ab159a1f0be3
hfhfff <dyftfboys@163.com>
2024-08-27 17:21:16 +0800

    创建了vivado文件

---
+++ blog.md
@@ -0,0 +1,7 @@
+新项目的github关联怎么创建：
+# 1
+在本地vivado文件夹内创建一个新的子文件P15
+# 2
+将文件上传到github上建立的repository上面
+# 3
+复制http到vscode中
---
+++ p14/p14.srcs/sources_1/new/send_byte.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/08/27 17:20:09
+// Design Name: 
+// Module Name: send_byte
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module send_byte(
+
+    );
+endmodule
